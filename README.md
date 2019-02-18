0cdf6410a6cf3d2ee6f133ff6a197b6924ef8c4c
Michal Maayan <micima125@gmail.com>
2019-02-18 13:05:08 +0200

    Adding geolocator file and update readme
    Adding  latitude and longitude python file

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ These files are in gitIgnore because of there sizes. They can be downloaded from
 https://www.kaggle.com/new-york-city/nyc-parking-tickets
 
 
+The python files need to be run in this order:
+1. creating_csv_files.py - create the csv fike with and without parking violation
+2. geolocator.py - creates a united csv file from the file with and without the parking viation which we created in the previous step. This file will include the street latitude and longitude
+3.
---
+++ geolocator.py
@@ -0,0 +1,136 @@
+"""
+Adding latitude and longitude to the parking ticket and the non violation files
+"""
+from geopy.geocoders import Nominatim
+import time
+import csv
+
+# The process of converting street name to its latitude and longitude takes some time,
+# like 4 hours, so we will keep the latitude and longitude we already found.
+address_cache = {}
+
+def address_to_coordinate(address):
+    """
+    replace street address to latitude and longitude
+    :param address:
+    :return:
+    """
+    address = address.lower()
+    if address in address_cache:
+        if address_cache[address] == None:
+            raise ValueError('bad address from cache')
+        return address_cache[address]
+    #geopi request to wait one second per request
+    time.sleep(1)
+    geolocator = Nominatim(user_agent="Python/3.6 "
+                                      "(michal.maayan+needleproject@mail.huji.ac.il) "
+                                      "Parking Tickets Research")
+    location = geolocator.geocode(address + " NYC")
+    # for not looking a bad address again
+    if location == None:
+        address_cache[address] = None
+        raise ValueError('bad address')
+    coordinate = (location.latitude, location.longitude)
+    address_cache[address] = coordinate
+    return coordinate
+
+def read_file(file_to_read, file_to_write):
+    """
+    Creates a new csv file based on the parking ticket violation csv file.
+    There are some streets name's which are incorrect so we ignore them.
+    :param file_to_read: the violation file we generated from the source csv
+    file using "creating_csv_files.py"
+    :param file_to_write: the new geo csv file with the corresponding latitude
+    and longitude
+    :return: number of lines in this file
+    """
+    with open(file_to_write, 'w', newline='') as filtered_file:
+        column_names = ["Day", "Street Name", "Latitude", "Longitude",
+                        "Violation Time", "Zone", "Parking Violation"]
+        writer = csv.DictWriter(filtered_file, fieldnames=column_names,
+                                extrasaction='ignore')
+        writer.writeheader()
+        num_lines = 0
+        num_filtered_lines = 0
+        with open(file_to_read) as original_file:
+            reader = csv.DictReader(original_file)
+            bad_address_counter = 0
+            for row in reader:
+                num_lines += 1
+                try:
+                    coordinate = address_to_coordinate(row["Street Name"])
+                except Exception as err:
+                    bad_address_counter+=1
+                    print("Got exception: %s" % err)
+                    print("Bad address: %s" % row["Street Name"])
+                    print("num of bad addresses - ",bad_address_counter)
+                    continue
+                row["Latitude"] = coordinate[0]
+                row["Longitude"] = coordinate[1]
+                row["Day"] = row["Issue Date"]
+                row["Parking Violation"] = row["Violation Description"]
+                writer.writerow(row)
+                num_filtered_lines += 1
+                if num_lines % 100 == 0:
+                    filtered_file.flush()
+                    print("current num of lines:", num_lines)
+                    print("current num of filtered lines:", num_filtered_lines)
+    return num_filtered_lines
+
+def add_to_file(file_to_read, file_to_write):
+    """
+    Adding to the geo file we created the lines with no violation
+    :param file_to_read: the no violation file we created in the previous
+    step, using - "creating_csv_files.py"
+    :param file_to_write: the geo file
+    :return: number of lines we added at this step
+    """
+    with open(file_to_write, 'a', newline='') as filtered_file:
+        column_names = ["Day", "Street Name", "Latitude", "Longitude",
+                        "Violation Time", "Zone", "Violation Description"]
+        writer = csv.DictWriter(filtered_file, fieldnames=column_names,
+                                extrasaction='ignore')
+        num_lines = 0
+        num_filtered_lines = 0
+        with open(file_to_read) as original_file:
+            reader = csv.DictReader(original_file)
+            bad_address_counter = 0
+            for row in reader:
+                num_lines += 1
+                try:
+                    coordinate = address_to_coordinate(row["Street Name"])
+                except Exception as err:
+                    bad_address_counter+=1
+                    print("Got exception: %s" % err)
+                    print("Bad address: %s" % row["Street Name"])
+                    print("num of bad address - ",bad_address_counter)
+                    continue
+                row["Latitude"] = coordinate[0]
+                row["Longitude"] = coordinate[1]
+                row["Day"] = row["Issue Date"]
+                writer.writerow(row)
+                num_filtered_lines += 1
+                if num_lines % 100 == 0:
+                    filtered_file.flush()
+                    print("current num of lines:", num_lines)
+                    print("current num of filtered lines:", num_filtered_lines)
+    return num_filtered_lines
+
+def main():
+    """
+    Read the violation and the non violation files and replace the street
+    name with the corresponding geolocation. Creates a united file name
+    "geo2016" or "geo2017" depend on the year of the input files.
+    """
+    then = time.time()
+    counter1 = read_file("Parking_Violations2017.csv", "geo2017.csv")
+    now = time.time()  # Time after it finished
+    print("It took for violation: ", now - then, " seconds")
+    print("now adding the no parking")
+    counter2 = add_to_file("no_parkingViolations2017.csv", "geo2017.csv")
+    now = time.time()  # Time after it finished
+    print("total lines in the united geo file:", counter1+counter2)
+    print("It took: ", now - then, " seconds")
+
+if __name__ == "__main__":
+    main()
