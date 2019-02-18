b1dc9bd8a4b3cc62c1bfb716ba01650e095416e0
Michal Maayan <micima125@gmail.com>
2019-02-18 13:19:01 +0200

    Create classification.py
    without the warning in random forest

---
+++ classification.py
@@ -0,0 +1,45 @@
+"""
+Now we would like to predict the likelihood of getting a parking ticket.
+We will train on the data we generated in the previous steps from 2016 and
+test on the one generated from 2017.
+"""
+import xgboost
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+import pandas as pd
+
+def classification_models(x_train, x_test, y_train, y_test):
+    classifiers = []
+    model1 = xgboost.XGBClassifier()
+    classifiers.append(model1)
+    model2 = RandomForestClassifier()
+    classifiers.append(model2)
+    model3 = tree.DecisionTreeClassifier()
+    classifiers.append(model3)
+
+    names = ["XGBClassifier", "RandomForestClassifier", "DecisionTreeClassifier"]
+    for i,clf in enumerate(classifiers):
+        clf.fit(x_train, y_train)
+        y_pred = clf.predict(x_test)
+        acc = accuracy_score(y_test, y_pred)
+        print("Accuracy of %s is %s" % (names[i], acc))
+
+def main():
+    #todo - insert the pdf file name
+    """
+    we will use 3 different classification algorithm. more about then in the
+    pdf.
+    """
+    training_data = pd.read_csv(
+        r"filtered_csv_files\geo2016.csv")
+    testing_data = pd.read_csv(
+        r"filtered_csv_files\geo2017.csv")
+    x_train = training_data[["Day", "Zone", "Latitude", "Longitude"]]
+    y_train = training_data['Violation Description']
+    x_test = testing_data[["Day", "Zone", "Latitude", "Longitude"]]
+    y_test = testing_data['Violation Description']
+    classification_models(x_train, x_test, y_train, y_test)
+
+if __name__ == "__main__":
+    main()
