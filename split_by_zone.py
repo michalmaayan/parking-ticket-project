dd659ddb2a2bad118b1006507a1f3a1f4baeccb7
Michal Maayan <micima125@gmail.com>
2019-02-20 17:17:52 +0200

    Update split_by_zone.py
    delete print

--- split_by_zone.py
+++ split_by_zone.py
@@ -23,18 +23,6 @@ def read_file(file_to_read, output_list):
     :param output_list:
     :return:
     """
-    num_lines_section1 = 0
-    num_lines_section2 = 0
-    num_lines_zone3 = 0
-    num_lines_zone4 = 0
-    num_lines_zone5 = 0
-    num_lines_zone6 = 0
-    num_lines_zone7 = 0
-    num_lines_zone8 = 0
-    num_lines_zone9 = 0
-    num_lines_zone10 = 0
-    num_lines_zone11 = 0
-    num_lines_zone12 = 0
     with open(file_to_read) as original_file:
         reader = csv.DictReader(original_file)
         for row in reader:
@@ -42,67 +30,41 @@ def read_file(file_to_read, output_list):
                 # zone between 7am to 9am
                 if 42 <= int(row["Zone"]) < 54:
                     add_lines(output_list[0], row)
-                    num_lines_section1+=1
                 # zone between 9am to 12pm
                 elif 54 <= int(row["Zone"]) < 72:
                     add_lines(output_list[1], row)
-                    num_lines_section2 += 1
                 # zone between 12pm to 16pm
                 elif 72 <= int(row["Zone"]) < 96:
                     add_lines(output_list[2], row)
-                    num_lines_zone3 += 1
                 # zone between 16pm to 19pm
                 elif 96 <= int(row["Zone"]) < 114:
                     add_lines(output_list[3], row)
-                    num_lines_zone4 += 1
                 # zone between 19pm to 23pm
                 elif 114 <= int(row["Zone"]) < 138:
                     add_lines(output_list[4], row)
-                    num_lines_zone5 += 1
                 # zone between 23pm to 07am
                 elif 138 <= int(row["Zone"]) or int(row["Zone"]) < 42:
                     add_lines(output_list[5], row)
-                    num_lines_zone6 += 1
             else:
                 #weekend
                 # zone between 7am to 9am
                 if 42 <= int(row["Zone"]) < 54:
                     add_lines(output_list[6], row)
-                    num_lines_zone7 += 1
                 # zone between 9am to 12pm
                 elif 54 <= int(row["Zone"]) < 72:
                     add_lines(output_list[7], row)
-                    num_lines_zone8 += 1
                 # zone between 12pm to 16pm
                 elif 72 <= int(row["Zone"]) < 96:
                     add_lines(output_list[8], row)
-                    num_lines_zone9 += 1
                 # zone between 16pm to 19pm
                 elif 96 <= int(row["Zone"]) < 114:
                     add_lines(output_list[9], row)
-                    num_lines_zone10 += 1
                 # zone between 19pm to 23pm
                 elif 114 <= int(row["Zone"]) < 138:
                     add_lines(output_list[10], row)
-                    num_lines_zone11 += 1
                 # zone between 23pm to 07am
                 elif 138 <= int(row["Zone"]) or int(row["Zone"]) < 42:
                     add_lines(output_list[11], row)
-                    num_lines_zone12 += 1
-
-        print("current num of lines zone1:", num_lines_section1)
-        print("current num of lines zone2:", num_lines_section2)
-        print("current num of lines zone3:", num_lines_zone3)
-        print("current num of lines zone4:", num_lines_zone4)
-        print("current num of lines zone5:", num_lines_zone5)
-        print("current num of lines zone6:", num_lines_zone6)
-        print("current num of lines zone7:", num_lines_zone7)
-        print("current num of lines zone8:", num_lines_zone8)
-        print("current num of lines zone9:", num_lines_zone9)
-        print("current num of lines zone10:", num_lines_zone10)
-        print("current num of lines zone11:", num_lines_zone11)
-        print("current num of lines zone12:", num_lines_zone12)
-
 
 def add_lines(output_file, row):
     """
@@ -149,7 +111,6 @@ def main():
         file_obj.close()
     now = time.time()  # Time after it finished
     print("It took: ", now - then1, " seconds")
-
 
 if __name__ == "__main__":
     main()
